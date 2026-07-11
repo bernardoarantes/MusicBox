ed978cddb13b8d2d01d1594a1c5df0ec61b474c4
Bernardo Arantes <bernardoarantes2006@gmail.com>
2026-07-11 20:29:31 -0300

    Created RegistryService

---
+++ backend/src/service/RegistryService.cpp
@@ -0,0 +1,24 @@
+#include <string>
+
+using std::string;
+using std::to_string;
+
+class RegistryService {
+    private:
+        unsigned user_count;
+        unsigned entry_count;
+
+    public:
+        RegistryService(unsigned user_count, unsigned entry_count) :
+            user_count(user_count),
+            entry_count(entry_count) {}
+
+        string generateUserId() {
+            return to_string(++entry_count);
+        }
+
+        string generateEntryId() {
+            return to_string(++user_count);
+        }
+};
+
