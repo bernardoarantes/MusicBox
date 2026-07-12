daa9c6318e51c296370976f48f863f35a607be04
Bernardo Arantes <bernardoarantes2006@gmail.com>
2026-07-11 23:16:09 -0300

    Created EndpointFactory

---
+++ backend/src/EndpointFactory.cpp
@@ -0,0 +1,26 @@
+#include "httplib.h"
+#include "exception"
+#include "service/handler/CreateEntryHandler.cpp"
+#include "service/UserService.cpp"
+
+using httplib::Request;
+using httplib::Response;
+using httplib::Server;
+using std::string;
+
+class EndpointFactory {
+    public:
+        EndpointFactory(Server &svr, EntryService &entryService, UserService &userService) {
+            loadEndpoints(svr, entryService, userService);
+        }
+
+    private:
+        void loadEndpoints(Server &svr, EntryService &entryService, UserService &userService) {
+            registerEndpoint(svr, new CreateEntryHandler(entryService), "/create-entry");
+        }
+
+        void registerEndpoint(Server &svr, Server::Handler *handler, string pattern) {
+            svr.Get(pattern, *handler);
+            new std::exception();
+        };
+};
