33499aff7cb0f85382711445644b2c62649e7bc5
Bernardo Arantes <bernardoarantes2006@gmail.com>
2026-07-12 14:08:37 -0300

    Registered new endpoints

--- backend/src/EndpointFactory.cpp
+++ backend/src/EndpointFactory.cpp
@@ -1,10 +1,15 @@
 #ifndef ENDPOINTFACTORY
 #define ENDPOINTFACTORY
 
-#include "httplib.h"
 #include "exception"
-#include "service/handler/CreateEntryHandler.cpp"
+#include "httplib.h"
 #include "service/UserService.cpp"
+#include "service/handler/AlbumQueryHandler.cpp"
+#include "service/handler/ArtistQueryHandler.cpp"
+#include "service/handler/CreateEntryHandler.cpp"
+#include "service/handler/CreateUserHandler.cpp"
+#include "service/handler/ListEntryHandler.cpp"
+#include "service/handler/MusicQueryHandler.cpp"
 
 using httplib::Request;
 using httplib::Response;
@@ -13,17 +18,22 @@ using std::string;
 
 class EndpointFactory {
     public:
-        EndpointFactory(Server &svr, EntryService &entryService, UserService &userService) {
-            loadEndpoints(svr, entryService, userService);
+        EndpointFactory(Server &svr, EntryService &entry_service, UserService &user_service, MusicQueryInterface &music_query_interface) {
+            loadEndpoints(svr, entry_service, user_service, music_query_interface);
         }
 
     private:
-        void loadEndpoints(Server &svr, EntryService &entryService, UserService &userService) {
-            registerEndpoint(svr, new CreateEntryHandler(entryService), "/create-entry");
+        void loadEndpoints(Server &svr, EntryService &entry_service, UserService &user_service, MusicQueryInterface &music_query_interface) {
+            registerEndpoint(svr, "/album-query",  AlbumQueryHandler(music_query_interface));
+            registerEndpoint(svr, "/artist-query", ArtistQueryHandler(music_query_interface));
+            registerEndpoint(svr, "/create-entry", CreateEntryHandler(entry_service));
+            registerEndpoint(svr, "/create-user",  CreateUserHandler(user_service));
+            registerEndpoint(svr, "/list-entry",   ListEntryHandler(entry_service));
+            registerEndpoint(svr, "/music-query",  MusicQueryHandler(music_query_interface));
         }
 
-        void registerEndpoint(Server &svr, Server::Handler *handler, string pattern) {
-            svr.Get(pattern, *handler);
+        void registerEndpoint(Server &svr, string pattern, Server::Handler handler) {
+            svr.Get(pattern, handler);
             new std::exception();
         };
 };
