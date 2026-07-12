96b0eb265d8c7b83cec577f7720aacb5e4950684
Bernardo Arantes <bernardoarantes2006@gmail.com>
2026-07-12 14:56:29 -0300

    Created backend entrypoint

---
+++ backend/src/main.cpp
@@ -0,0 +1,35 @@
+#include "httplib.h"
+#include "EndpointFactory.cpp"
+#include "service/RegistryService.cpp"
+#include "service/UserParser.cpp"
+#include "service/UserService.cpp"
+#include "service/EntryParser.cpp"
+#include "service/EntryService.cpp"
+#include "service/MusicQueryInterface.cpp"
+#include "service/SpotifyAPIQueryService.cpp"
+#include "service/persistence/Repository.cpp"
+
+#define ENTRIES_PATH "data/entries.json"
+#define USERS_PATH "data/users.json"
+#define API_KEY "SOME_KEY"
+
+int main() {
+    httplib::Server svr;
+
+
+    Repository users(USERS_PATH, UserParser());
+    Repository entries(ENTRIES_PATH, EntryParser());
+
+    Workspace workspace(users, entries);
+    RegistryService registry(users.size(), entries.size());
+
+    UserService user_service(workspace, registry);
+    EntryService entry_service(workspace, registry);
+
+    SpotifyAPIQueryService spotify_api_query_service(API_KEY);
+
+    EndpointFactory(svr, entry_service, user_service, spotify_api_query_service);
+
+    svr.listen("0.0.0.0", 8080);
+}
+
