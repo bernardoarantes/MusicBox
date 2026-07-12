7a9513e4a28c173f5189b55033e6c061285d3bc6
Bernardo Arantes <bernardoarantes2006@gmail.com>
2026-07-12 13:09:23 -0300

    Integrated Repository into Workspace

--- backend/src/service/Workspace.cpp
+++ backend/src/service/Workspace.cpp
@@ -1,15 +1,17 @@
 #ifndef WORKSPACE
 #define WORKSPACE
-#include "exception"
+
 #include "entity/EntryEntity.cpp"
 #include "entity/UserEntity.cpp"
+#include "exceptions/EntityNotFoundException.cpp"
 #include "json.hpp"
+#include "service/persistence/Repository.cpp"
+#include "service/UserParser.cpp"
+#include "service/EntryParser.cpp"
 #include <map>
-#include <string>
-#include <fstream>
-#include <vector>
 #include <stdexcept>
-#include "exceptions\EntityNotFoundException.cpp"
+#include <string>
+#include <vector>
 
 using std::map;
 using std::string;
@@ -17,51 +19,48 @@ using std::vector;
 
 class Workspace {
     private:
-        vector<UserEntity*> users = vector<UserEntity*>();
-        vector<EntryEntity*> entries = vector<EntryEntity*>();
-        string path_users;
-        string path_entries;
-        UserEntity findUser(const string& id){
-            for (UserEntity* user: users) {
-                if (user->getId() == id) return *user;
+        Repository users;
+        Repository entries;
+
+        const UserEntity getUser(const string& user_id){
+            for (const Entity *user : users) {
+                if (user->getId() == user_id) {
+                    return *((UserEntity *) user);
+                }
             }
-        };
-        EntryEntity findEntry(const string& id){
-            for (EntryEntity* entry: entries) {
-                if (entry->getId() == id) return *entry;
-            }
-        };
-    public:
-        Workspace(const string& path_u, const string& path_e) {
-            path_users = path_u;
-            path_entries = path_e;
-            try{
-            std::ifstream arc(path_users);
-            if(!arc.is_open()) throw EntityNotFound("Caminho do Arquivo de Users não pôde ser aberto");
-            arc.close();
-            } catch(const EntityNotFound& e){
-            }
-            try{
-            std::ifstream arc(path_entries);
-            if(!arc.is_open()) throw EntityNotFound("Caminho do Arquivo de Reviews não pôde ser aberto");
-            arc.close();
-            } catch(const EntityNotFound& e){
-            }
+            throw new EntityNotFoundException("Bad user id " + user_id);
         }
+
+        const EntryEntity getEntry(const string &entry_id) {
+            for (const Entity *entry : entries) {
+                if (entry->getId() == entry_id) {
+                    return *((EntryEntity *) entry);
+                }
+            }
+            throw new EntityNotFoundException("Bad entry id " + entry_id);
+        }
+
+    public:
+        Workspace(const string &users_path, const string &entries_path, const UserParser &user_parser, const EntryParser &entry_parser) :
+            users(users_path, user_parser),
+            entries(entries_path, entry_parser) {}
+
         void addEntry(const string &user_id, const string &entry_id, const string &type, const string &target_id, const string &comment, unsigned rating) {
-            entries.push_back(new EntryEntity(entry_id, type, target_id, comment, rating));
+            entries.save(new EntryEntity(entry_id, user_id, type, target_id, comment, rating));
         }
 
         void addUser(const string &user_id, const string &name, const string &password) {
-            users.push_back(new UserEntity(user_id, name, password));
+            users.save(new UserEntity(user_id, name, password));
         }
 
-        vector<EntryEntity>& findAllUserEntries(const string& id){
-            vector<EntryEntity> v = vector<EntryEntity>();
-            for (EntryEntity* entry: entries) {
-                if(entry->getOwnerId() == id) v.push_back(*entry);
+        vector<EntryEntity> *listEntriesByUser(const string& user_id){
+            vector<EntryEntity> *user_entries = new vector<EntryEntity>();
+            for (const Entity *entry : entries) {
+                EntryEntity *_entry = (EntryEntity *) entry;
+                if (_entry->getOwnerId() == user_id)
+                    user_entries->push_back(*_entry);
             }
-            return v;
+            return user_entries;
         };
 };
 
