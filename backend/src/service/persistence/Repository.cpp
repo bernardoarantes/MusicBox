1668cc3dc38ba83c3048f8fefefb1b8acf81f190
Bernardo Arantes <bernardoarantes2006@gmail.com>
2026-07-12 13:05:44 -0300

    Created Repository persistence service

---
+++ backend/src/service/persistence/Repository.cpp
@@ -0,0 +1,72 @@
+#ifndef REPOSITORY
+#define REPOSITORY
+
+#include "entity/Entity.cpp"
+#include "exceptions/EntityNotFoundException.cpp"
+#include "service/EntityService.cpp"
+#include "service/EntityParser.cpp"
+#include <filesystem>
+#include <fstream>
+#include <ios>
+#include <string>
+#include <vector>
+
+using std::fstream;
+using std::ios;
+using std::string;
+using std::vector;
+
+class Repository {
+    private:
+        fstream file;
+        vector<const Entity *> entities;
+
+    public:
+        Repository(const string &file_path, const EntityParser &entity_parser) {
+            fstream in_file;
+            in_file.open(file_path, ios::in);
+
+            if (!in_file.is_open())
+                throw EntityNotFoundException("Bad input file path " + file_path);
+            
+            json data;
+            file >> data;
+
+            if (!std::filesystem::is_empty(file_path))
+                in_file >> data;
+
+            in_file.close();
+
+            for (json j : data)
+                entities.push_back(entity_parser.parseJson(j));
+
+            file.open(file_path, ios::app);
+        }
+
+        const Entity &findById(const string &id) const {
+            for (const Entity *entity : entities)
+                if (entity->getId() == id) 
+                    return *entity;
+
+            throw new EntityNotFoundException("Bad id " + id);
+        }
+
+        vector<const Entity *> findAll() const {
+            return entities;
+        }
+
+        void save(const Entity *entity) {
+            entities.push_back(entity);
+            file << "," + entity->toJson().dump();
+        }
+
+        vector<const Entity *>::iterator begin() {
+            return entities.begin();
+        }
+
+        vector<const Entity *>::iterator end() {
+            return entities.end();
+        }
+};
+
+#endif
